dc06d4d078541df2e12c63de58d87253f0fb983a
kishore1085 <kishoreperf2018@gmail.com>
2025-03-11 21:52:46 +0530

    change2

---
+++ variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Vaarible1 = kishore
+variable2 = Devisetti
+
+echo "$variable1 is a good boy and he can capable of doing good things "
+echo "$variable2 is a ok but he will not  make noise "
