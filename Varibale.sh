c35295b99fc755e08f5947dfb0c9259277ebbf1e
kishore1085 <kishoreperf2018@gmail.com>
2025-03-11 21:46:08 +0530

    change #2

--- Varibale.sh
+++ Varibale.sh
@@ -2,5 +2,5 @@
 Vaarible1 = kishore
 variable2 = Devisetti
 
-$variable1 is a good boy and he can capable of doing good things 
-$variable2 is a ok but he will not  make noise 
+echo "$variable1 is a good boy and he can capable of doing good things "
+echo "$variable2 is a ok but he will not  make noise "
